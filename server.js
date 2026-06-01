665f35b09fbcc89f6d996d7f9bc88a1bf43d6ead
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-06-01 19:24:14 +0200

    #4 Opzetten server.js en mappenstructuur
    Het volgende is aangepast/toegevoegd:
    - npm install
    - style/styleguide bestanden uit head toegevoegd
    - server.js ingesteld
    - route voor index gemaakt
    - data uit "houses" endpoint gefetched en geparsed
    - index gecreërd

--- server.js
+++ server.js
@@ -1,1 +1,45 @@
-console.log('Hier komt je server voor Sprint 12.')
+// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
+// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
+import express from "express";
+
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from "liquidjs";
+
+// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+const app = express();
+
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({ extended: true }))
+
+// Gebruik de map 'public' voor statische bestanden
+app.use(express.static("public"));
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine("liquid", engine.express());
+
+// Stel de map met Liquid templates in
+app.set("views", "./views");
+
+// Stel het poortnummer in waar Express op moet gaan luisteren
+app.set("port", process.env.PORT || 8000);
+
+// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  console.log(`Project draait via http://localhost:${app.get('port')}`)
+})
+
+const baseURL = 'https://fdnd-agency.directus.app/items/f_'
+
+// Creer een route voor de index en fetch en parse data
+app.get('/', async function (request, response) {
+
+  // https://fdnd-agency.directus.app/items/f_houses
+  const housesResponse = await fetch(baseURL + "houses");
+  const housesResponseJson = await housesResponse.json();
+
+  // Exporteer data naar de index
+  response.render('index.liquid', {
+    houses: housesResponseJson.data
+  });
+});
