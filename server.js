43c842bb2b565eac13ad5f3d651849957bc5fdd5
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-06-08 11:13:39 +0200

    #4 custom "dynamic" values added

--- server.js
+++ server.js
@@ -38,8 +38,31 @@ app.get('/', async function (request, response) {
   const housesResponse = await fetch(baseURL + "houses");
   const housesResponseJson = await housesResponse.json();
 
+  // Array aangemaakt voor de data
+  let houses = []
+
+  // Voor elke price property Value, maak een custom property voor en pas Numberformat() aan toe
+  housesResponseJson.data.forEach(function (house) {
+    house.customPrice = new Intl.NumberFormat('nl-NL', { style: 'currency', currency: 'EUR' }).format(house.price)
+
+    const customDates = ['Een week', 'Twee weken', 'Langer dan drie weken']
+    house.customDate = customDates[Math.floor(Math.random() * customDates.length)]
+
+    const customAanvaarding = ['Kan snel', 'In overleg', 'Leeg en ontruimd']
+    house.customAanvaarding = customAanvaarding[Math.floor(Math.random() * customAanvaarding.length)]
+
+    const customVraagprijsPerM = '€ 4.774';
+    house.customVraagprijsPerM = customVraagprijsPerM;
+
+    const customStatus = ['Beschikbaar', 'Niet beschikbaar']
+    house.customStatus = customStatus[Math.floor(Math.random() * customStatus.length)]
+
+    houses.push(house)
+  })
+
+
   // Exporteer data naar de index
   response.render('index.liquid', {
-    houses: housesResponseJson.data
+    houses: houses
   });
 });
