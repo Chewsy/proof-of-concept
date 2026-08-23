743280cacd74814dbed84e515c34b43b295a0818
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-08-23 21:20:16 +0200

    #11 card partial elementen genest in een <a>
    Deze commit bevat:
    
    - een functie om data op te halen van het geselecteerde huis (voor reparatie gemaakt)
    - href op de index die leidt naar de /huis route + opgehaalde houde.id waarde
    - de detailpagina voor het geselecteerde huis (voor reparatie gemaakt)
    - simepele styling voor de nieuwe anchor element in list_card.liquid

--- server.js
+++ server.js
@@ -172,3 +172,78 @@ app.post('/favoriet', async function (request, response) {
 
   response.redirect('/');
 });
+
+// Deze functie wordt dus uitgevoerd als ik op een woning klik uit de overzichtspagina (index)
+app.get('/huis/:id', async function (request, response) {
+
+  // https://fdnd-agency.directus.app/items/f_houses
+  const housesResponse = await fetch(baseURL + "houses");
+  const housesResponseJson = await housesResponse.json();
+
+  const listResponse = await fetch(baseURL + "list/20");
+  const listResponseJson = await listResponse.json();
+
+  const favorieteHuizenIds = listResponseJson.data ? listResponseJson.data.houses : [];
+
+  // Array aangemaakt voor de data
+  let houses = []
+
+  // Voor elke price property Value, maak een custom property voor en pas Numberformat() aan toe
+  housesResponseJson.data.forEach(function (house) {
+    house.customPrice = new Intl.NumberFormat('nl-NL', { style: 'currency', currency: 'EUR' }).format(house.price)
+
+    const customDatum = ['Een week', 'Twee weken', 'Langer dan drie weken']
+    house.customDatum = customDatum[Math.floor(Math.random() * customDatum.length)]
+
+    const customAanvaarding = ['Kan snel', 'In overleg', 'Leeg en ontruimd']
+    house.customAanvaarding = customAanvaarding[Math.floor(Math.random() * customAanvaarding.length)]
+
+    const customVraagprijsPerM = '€ 4.774';
+    house.customVraagprijsPerM = customVraagprijsPerM;
+
+    const customStatus = ['Beschikbaar', 'Niet beschikbaar']
+    house.customStatus = customStatus[Math.floor(Math.random() * customStatus.length)]
+
+    const customBouwJaar = ['1998', '2024', '2000', '2001', '1993', '1999']
+    house.customBouwJaar = customBouwJaar[Math.floor(Math.random() * customBouwJaar.length)]
+
+    const customHHuisType = ['Appartement', 'Vrijstaande woning', 'Rijtjeshuis', 'Eengezinswoning', 'Portiekflat']
+    house.customHHuisType = customHHuisType[Math.floor(Math.random() * customHHuisType.length)]
+
+    const customBouwType = ['Nieuwbouw', 'Bestaande bouw']
+    house.customBouwType = customBouwType[Math.floor(Math.random() * customBouwType.length)]
+
+    const customDakType = 'Zadeldak';
+    house.customDakType = customDakType;
+
+    const customEnergielabel = ['A++++', 'A+++', 'A++', 'A+', 'B', 'C', 'D', 'E', 'F', 'G']
+    house.customEnergielabel = customEnergielabel[Math.floor(Math.random() * customEnergielabel.length)]
+
+    const maxBekeken = 10000;
+    const maxBewaard = 50;
+
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max) + 1;
+    }
+
+    function getRandomInt(maxBekeken) {
+      return Math.floor(Math.random() * maxBekeken) + 1;
+    }
+
+    function getRandomInt(maxBewaard) {
+      return Math.floor(Math.random() * maxBewaard) + 1;
+    }
+
+    house.randomBekeken = getRandomInt(maxBekeken);
+    house.randomBewaard = getRandomInt(maxBewaard);
+
+    houses.push(house)
+  })
+
+
+  // Exporteer data naar de index
+  response.render('huis.liquid', {
+    houses: houses,
+    favorieteHuizenIds: favorieteHuizenIds
+  });
+});
